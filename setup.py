5b6eb9bdb1987049277b09868b5cc4e3973fa62f
Christian Hill <xn.hill@gmail.com>
2021-12-01 14:34:11 +0100

    Now version 0.5

--- setup.py
+++ setup.py
@@ -6,10 +6,17 @@ Import of article metadata from DOI and Bibcode is supported.
 
 See https://github.com/xnx/django-pyref for more information.
 """
+# Read in dependencies list from requirements.txt
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name = 'django-pyref',
-    version = '0.4.0',
+    version = '0.5.0',
     author = 'Frances Skinner, Iouli Gordon, Christian Hill, Robert Hargreaves, Kelly Lockhart',
     author_email = 'xn.hill@gmail.com',
     description = 'A package for managing bibliographic references',
@@ -18,6 +25,7 @@ setup(
     include_package_data=True,
     url = 'https://github.com/xnx/django-pyref',
     packages = find_packages(),
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
